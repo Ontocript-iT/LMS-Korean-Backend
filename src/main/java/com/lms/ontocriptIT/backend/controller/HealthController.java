3a1291a3bddada05ef5cac102dcc45dda40b142c
thilina22 <test@123gmail.com>
2026-01-28 12:24:36 +0530

    up

---
+++ src/main/java/com/lms/ontocriptIT/backend/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.lms.ontocriptIT.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Active";
+    }
+}
